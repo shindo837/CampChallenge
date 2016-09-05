7ff56179dae789e3cffaec528ab6c578d9478674
VAIO\tomoya <tomoya.g.shindo@gmail.com>
2016-09-05 23:28:18 +0900

    ３の倍数と３の付く数字のときアホになる。 ５の倍数のとき犬っぽくなる。 ３と５の倍数のときアホ犬になる。

---
+++ camp6.php
@@ -0,0 +1,23 @@
+﻿<?php
+$a=0;
+$b=0;
+for($i=1;$i<4001;$i++){
+	echo "$i:";
+		if((strpos($i,'3')!==false || $i%3==0) && $i%5==0){
+			$c=$a+$b;
+			echo "[アホ犬になる,アホ犬度$c]";
+		}
+		else if($i%5==0){
+			$b++;
+			echo "[犬っぽくなる,犬度$b]";
+		}
+		else if(strpos($i,'3')!==false || $i%3==0){
+			$a++;
+			echo "[アホになる,アホ度$a]";
+		}
+/*		else if($i%5 != 0){
+			echo " ";
+	}*/
+	echo "<br />";
+}
+?>
